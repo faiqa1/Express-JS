f57125cab2dd47613ec2b3dff9419269981ab8c3
faiqa1 <faiqarao03@gmail.com>
2022-09-16 17:55:55 +0500

    chng

--- test.js
+++ test.js
@@ -3,4 +3,10 @@ const express = require('express');
 const port = 2410;
 
 const app = express();
+
+app.get("/svr/test",function(req,res)
+{
+    res.send("successful");
+});
+
 app.listen(`listening at port  ${port}`);
