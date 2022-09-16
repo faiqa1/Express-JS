e26c8a4443f742dd6810e21f640aa78ca0950a87
faiqa1 <faiqarao03@gmail.com>
2022-09-16 17:38:59 +0500

    jjs

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(function(req,res, next){
 
     next();
 });
-const port = 1234;
+const port = 2410;
 app.listen(port, () => console.log(`listening at port  ${port}!`) );
  
 let {data} = require("./students");
